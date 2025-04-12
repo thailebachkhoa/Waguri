cfc85bbb4cb0ae357474b2475309f7374c6f2bc8
waguri <thai.lebachkhoa@hcmut.edu.vn>
2025-04-12 17:09:18 +0700

    add thêm thông tin cho queue và sched

--- src/queue.c
+++ src/queue.c
@@ -7,7 +7,7 @@ int empty(struct queue_t *q)
         if (q == NULL)
         {
                 printf("Queue is NULL\n");
-                return -1;
+                return 0;
         }
         return (q->size == 0);
 }
@@ -15,8 +15,8 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
         /* TODO: put a new process to queue [q] */
-        if (q->size < MAX_QUEUE_SIZE)
-                q->proc[q->size++] = proc;
+        if (q->size < MAX_QUEUE_SIZE) q->proc[q->size++] = proc;
+        else printf("Fail to enqueue process %s\n", proc->path);
 }
 
 struct pcb_t *dequeue(struct queue_t *q)
@@ -26,21 +26,19 @@ struct pcb_t *dequeue(struct queue_t *q)
          * */
 
         // Kiểm tra ngoại lệ
-        if (empty(q))
-                return NULL;
+        if (empty(q)) return NULL;
 
-        // Tìm priority cao nhất trong queue [q]
-        int highestIndex = 0;
+        // Tìm index trong pcb có priority cao nhất trong queue [q]
+        int toFindIndex = 0;
         for (int i = 1; i < q->size; i++)
         {
-                if (q->proc[i]->priority < q->proc[highestIndex]->priority)
-                {
-                        highestIndex = i;
-                }
+                int highestPriority = q->proc[toFindIndex]->priority;
+                int currentPriority = q->proc[i]->priority;
+                if (currentPriority < highestPriority) toFindIndex = i; 
         }
         // Xóa process có priority cao nhất
-        struct pcb_t *process = q->proc[highestIndex];
-        for (int i = highestIndex; i < q->size - 1; i++)
+        struct pcb_t *process = q->proc[toFindIndex];
+        for (int i = toFindIndex; i < q->size - 1; i++)
         {
                 // Di chuyển các process còn lại về trước
                 q->proc[i] = q->proc[i + 1];
