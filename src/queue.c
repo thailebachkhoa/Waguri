2b4d3e2520fb7dafdde89f86de2833918959e1d6
waguri <thai.lebachkhoa@hcmut.edu.vn>
2025-04-13 22:00:36 +0700

    test queue

--- src/queue.c
+++ src/queue.c
@@ -1,38 +1,98 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "queue.h"
+
+// int empty(struct queue_t * q) {
+//         if (q == NULL) return 1;
+// 	return (q->size == 0);
+// }
+
+// void enqueue(struct queue_t * q, struct pcb_t * proc) {
+//         /* TODO: put a new process to queue [q] */
+//         if(q->size < MAX_QUEUE_SIZE){
+//                 q->proc[q->size++] = proc;
+//         }
+// }
+
+// struct pcb_t * dequeue(struct queue_t * q) {
+//         /* TODO: return a pcb whose prioprity is the highest
+//          * in the queue [q] and remember to remove it from q
+//          * */
+// 	if(empty(q)){
+//                 return NULL;
+//                }
+//                int highIndex = 0;
+//                for(int i = 1;i < q->size;i++){
+//                 if(q->proc[i]->priority < q->proc[highIndex]->priority){
+//                         highIndex = i;
+//                 }
+        
+//                }
+//                struct pcb_t * process = q->proc[highIndex];
+//                for(int i = highIndex;i < q->size - 1;i++){
+//                 q->proc[i] = q->proc[i+1];
+//                }
+//                q->size--;
+//                return process;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
 
 int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
-	return (q->size == 0);
+if (q == NULL) return 1;
+return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
-        /* TODO: put a new process to queue [q] */
-        if(q->size < MAX_QUEUE_SIZE){
-                q->proc[q->size++] = proc;
-        }
+
+
+
+/* TODO: put a new process to queue [q] */
+void enqueue(struct queue_t * q, struct pcb_t * proc){
+if(q == NULL || proc == NULL) return;
+if(q->size >= MAX_QUEUE_SIZE) return;
+
+q->proc[q->size] = proc;
+++q->size;
 }
 
+
+
+
+/* TODO: return a pcb whose prioprity is the highest
+* in the queue [q] and remember to remove it from q
+* */
 struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	if(empty(q)){
-                return NULL;
-               }
-               int highIndex = 0;
-               for(int i = 1;i < q->size;i++){
-                if(q->proc[i]->priority < q->proc[highIndex]->priority){
-                        highIndex = i;
-                }
-        
-               }
-               struct pcb_t * process = q->proc[highIndex];
-               for(int i = highIndex;i < q->size - 1;i++){
-                q->proc[i] = q->proc[i+1];
-               }
-               q->size--;
-               return process;
+if(empty(q)) return NULL;
+
+int queue_size = q->size;
+int idx = 0;
+uint32_t temp = q->proc[0]->priority;
+uint32_t current_prio;
+
+for(int a = 1; a < queue_size; ++a){
+if(q->proc[a] == NULL) continue;
+
+//check prio highest (which value is lowest)
+current_prio = q->proc[a]->priority;
+if(current_prio < temp){
+temp = current_prio;
+idx = a;
+}
+}
+
+
+// save
+struct pcb_t* return_pcb_t_pointer = q->proc[idx];
+
+// sort queue (overwrite idx value)
+for(int a = idx + 1; a < queue_size; ++a){
+q->proc[a - 1] = q->proc[a];
+}
+q->proc[queue_size - 1] = NULL;
+--(q->size);
+
+return return_pcb_t_pointer;
 }
 
