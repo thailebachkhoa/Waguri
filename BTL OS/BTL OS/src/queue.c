f7453326f28d37b01b5e5062fc0d9810e82213eb
waguri <thai.lebachkhoa@hcmut.edu.vn>
2025-04-15 20:41:00 +0700

    test

--- BTL OS/BTL OS/src/queue.c
+++ BTL OS/BTL OS/src/queue.c
@@ -2,30 +2,50 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
-	return (q->size == 0);
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+        {
+                fprintf(stderr, "Error: queue is NULL\n");
+                return -1;
+        }
+        return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
         /* TODO: put a new process to queue [q] */
+
+        // exception handling
+        if (q == NULL)
+        {
+                fprintf(stderr, "Error: queue is NULL\n");
+                return;
+        }
+        if (proc == NULL)
+        {
+                fprintf(stderr, "Error: process is NULL\n");
+                return;
+        }
         if (q->size >= MAX_QUEUE_SIZE)
                 return;
-        q->proc[q->size] = proc;
-        q->size++;      
-}       
-
-struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	// return NULL;
-        if (q->size == 0)
-                return NULL;
-        struct pcb_t *proc = q->proc[0];
-        for (int i = 1; i < q->size; i++)
-                q->proc[i - 1] = q->proc[i];
-        q->size--;
-        return proc;
+        // main action
+        q->proc[q->size++] = proc;
 }
 
+struct pcb_t *dequeue(struct queue_t *q)
+{
+        // exception handling
+        if (empty(q))
+        {
+                fprintf(stderr, "Error: queue is empty\n");
+                return NULL;
+        }
+
+        // shift the queue
+        struct pcb_t *proc = q->proc[0];
+        for (int i = 1; i < q->size; i++) q->proc[i - 1] = q->proc[i];
+        q->size--;
+        q->proc[q->size] = NULL; // clear the last element
+        return proc;
+}
